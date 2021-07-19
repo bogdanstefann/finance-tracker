72b590997fa03f20b14e8a9ef460ac66da5f2902
bogdanstefann <bogdanstefann@gmail.com>
2021-07-19 13:07:02 +0300

    Added search bar with partial rendering using ajax

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   get root 'welcome#index'
 
+  get 'search_stock', to: 'stocks#search'
+
+  get 'my_portfolio', to: 'users#my_portfolio'
+
 end
